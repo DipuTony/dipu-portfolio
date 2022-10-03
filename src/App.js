70b667a6df38d53847129bea8e7eda3e6bb493cc
Dipu Singh <dsingh197@gmail.com>
2022-10-04 01:08:24 +0530

    theme dark / light mode working..

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
+/////////////////////////////////////////////
+//  
+//  Author :- Dipu Kumar Singh
+//  Project :- Dipu Porfolio
+//  Date : - 03 - Oct - 2022
+//  Component :- App.js
+//  Note : -
+//
+/////////////////////////////////////////////
+
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Header from './Components/Common/Header/Header';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import Home from './Pages/Home';
+import Projects from './Pages/Projects';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={< Home />} />
+          <Route path="/about" element={< About />} />
+          <Route path="/contact" element={< Contact />} />
+          <Route path="/projects" element={< Projects />} />
+        </Routes>
+      </Router>
+
+    </>
   );
 }
 
---
+++ src/Components/Common/Header/Header.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { TbBrightnessUp } from 'react-icons/tb';
+import { MdDarkMode } from 'react-icons/md';
+
+function Header() {
+
+    const [theme, setTheme] = useState('light')
+
+    useEffect(() => {
+        document.body.className = theme;
+    }, [theme])
+
+    const handleModeButton = () => {
+        if (localStorage.getItem('mode') == 'dark') {
+            localStorage.setItem('mode', 'light')
+            setTheme("light")
+        } else {
+            localStorage.setItem('mode', 'dark')
+            setTheme('dark')
+        }
+
+    }
+
+    return (
+        <>
+            <div className='dark w-screen h-10 dark:text-white text-black dark:bg-slate-600 bg-sky-200'>
+                <nav className='flex justify-center'>
+                    <Link className='px-2' to="/">Home</Link>
+                    <Link className='px-2' to="/about">About</Link>
+                    <Link className='px-2' to="/contact">Contact</Link>
+                    <Link className='px-2' to="/projects">Projects</Link>
+                </nav>
+                <div className='flex justify-end mr-10 -mt-3'>
+                    {theme == 'light' && <TbBrightnessUp size={25} className="cursor-pointer" onClick={handleModeButton} />}
+                    {theme !='light' && <MdDarkMode size={25} className="cursor-pointer" onClick={handleModeButton} />}
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Header
