ccaf3eb249ad836e0027886d94abab2cee889343
Dipu Singh <dsingh197@gmail.com>
2022-10-04 23:19:20 +0530

    Home About simple

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -2,7 +2,22 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <>
+      <div className='min-h-screen bg-white dark:bg-gray-900 dark:text-gray-300 text-gray-900'>
+        <div className='pt-16 p-10'>
+
+          <div className='grid grid-cols-12 p-10'>
+            <div className='md:col-span-4 col-span-12'>
+              <img className='h-72 w-72 rounded-md' src="https://avatars.githubusercontent.com/u/9476513?v=4" alt="Dipu Singh" />
+            </div>
+            <div className='md:col-span-8 col-span-12 px-5'>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque consequuntur necessitatibus eligendi reiciendis praesentium a culpa deleniti soluta quasi?</p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import { FcDownload } from 'react-icons/fc';
 
 function Home() {
   return (
     <>
-      <div className=' min-h-screen overflow-x-auto bg-white dark:text-gray-400 dark:bg-gray-900'>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis, nihil!
+      <div className=' min-h-screen overflow-x-auto bg-white dark:text-white dark:bg-gray-900'>
+        <div className='pt-20'>
+          <div className='grid grid-cols-12'>
+            <div className='col-span-5 ml-20 mt-10'>
+              <p className='text-2xl font-semibold'>Hi,  I am<br /> <span className='text-5xl'> Dipu Kumar Singh</span> </p>
+              <p className='py-3 font-serif text-2xl'>Frontend Developer</p>
+              <div className='py-9'>
+                <button className='flex px-5 py-3 font-semibold dark:bg-gray-200 bg-gray-800 text-white dark:text-gray-800 rounded-sm shadow-xl'><FcDownload className='mt-1 mx-2' /> Download CV</button>
+              </div>
+            </div>
+            <div className='col-span-7 p-40 -mt-36'>
+              <p><img src="https://react-tailwindcss-portfolio.netlify.app/static/media/developer-dark.3f07bd13.svg" alt="" /></p>
+            </div>
+          </div>
+
+        </div>
       </div>
     </>
   )
